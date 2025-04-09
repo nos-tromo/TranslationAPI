e782f3e4fec97cea5490cc389c94715a46da251f
nos-tromo <ohhimarc@proton.me>
2025-04-09 21:16:07 +0200

    Refactor main logic
    - Reorganize imperative code into functions
    - Implement error handling and logging

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,22 @@
-import json
+import logging
+from datetime import datetime
 from pathlib import Path
+
+logfile_name = f"translator_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+logs_dir = Path(".logs")
+logs_dir.mkdir(parents=True, exist_ok=True)
+logfile_path = logs_dir / logfile_name
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler(logfile_path),
+        logging.StreamHandler()
+    ]
+)
+
+
+import json
 from pydantic import BaseModel
 
 from fastapi import FastAPI
@@ -7,8 +24,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from translator import Translator
 
-app = FastAPI(title="NLLB-200 Translation API")
 
+app = FastAPI(title="NLLB-200 Translation API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,13 +33,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-# Load languages
-language_path = Path(__file__).parent / "language_codes.json"
-with open(language_path, "r", encoding="utf-8") as f:
-    LANGUAGE_NAMES = json.load(f)
-
-# Initialize translator
 translator = Translator()
 
 
@@ -32,12 +42,28 @@ class TranslationRequest(BaseModel):
     target_lang: str
 
 
+def _load_language_codes(filename: str = "language_codes.json") -> dict[str, str] | None:
+    try:
+        language_path = Path(__file__).parent / filename
+        with open(language_path, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        logging.error(f"Error loading language code file: {e}")
+
+
 @app.post("/translate")
 def translate(req: TranslationRequest):
-    result = translator.translate(req.text, req.source_lang, req.target_lang)
-    return {"translation": result}
+    try:
+        result = translator.translate(req.text, req.source_lang, req.target_lang)
+        return {"translation": result}
+    except Exception as e:
+        logging.error(f"Error on /translate endpoint: {e}")
 
 
 @app.get("/languages")
 def get_languages():
-    return [{"code": code, "name": LANGUAGE_NAMES.get(code, code)} for code in LANGUAGE_NAMES]
+    try:
+        LANGUAGE_NAMES = _load_language_codes()
+        return [{"code": code, "name": LANGUAGE_NAMES.get(code, code)} for code in LANGUAGE_NAMES]
+    except Exception as e:
+        logging.error(f"Error on /languages endpoint: {e}")
